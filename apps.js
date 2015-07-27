8a40064f3a64e234429ad48ec20c06a41d1aeec8
swaption2009 <swaption2009@gmail.com>
2015-07-27 14:57:25 -0700

    Add Knockout custom binding for Google Maps
    to do: replace fake data with restaurant data

--- apps.js
+++ apps.js
@@ -1,6 +1,47 @@
+// Knockout Custom Binding for Google Maps
+function point(name, lat, long) {
+    this.name = name;
+    this.lat = ko.observable(lat);
+    this.long = ko.observable(long);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, long),
+        title: name,
+        map: map,
+        draggable: true
+    });
+
+    //if you need the poition while dragging
+    google.maps.event.addListener(marker, 'drag', function() {
+        var pos = marker.getPosition();
+        this.lat(pos.lat());
+        this.long(pos.lng());
+    }.bind(this));
+
+    //if you just need to update it when the user is done dragging
+    google.maps.event.addListener(marker, 'dragend', function() {
+        var pos = marker.getPosition();
+        this.lat(pos.lat());
+        this.long(pos.lng());
+    }.bind(this));
+}
+
+var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 13,
+    center: new google.maps.LatLng(37.7833, -122.4167),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+
 // Knockout ViewModel
 var ViewModel = function() {
 	var self = this;
+
+	self.points = ko.observableArray([
+		new point('Test1', 37.7833, -122.4167),
+		new point('Test2', 37.7843, -122.4167),
+		new point('Test3', 37.7833, -122.4177)
+	]);
 
 	// Non-editable data from server
 	self.Restaurants = ko.observableArray([

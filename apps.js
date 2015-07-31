a5e543b1a57a8da785803bb328141614b3f34f24
swaption2009 <swaption2009@gmail.com>
2015-07-30 17:12:27 -0700

    Add infoWindow inside scope to each marker
    http://stackoverflow.com/questions/3158598/google-maps-api-v3-adding-an-
    infowindow-to-each-marker

--- apps.js
+++ apps.js
@@ -40,48 +40,30 @@ var ViewModel = function() {
 
 	// Create marker from each restaurant's lat & long
 	for (var i = 0; i < self.filteredRestaurants().length; i ++) {
-		console.log(self.filteredRestaurants()[i].lat);
-		// console.log(self.restaurants()[i].lat);
-		// console.log(self.restaurants()[i].long);
-		var lat = self.filteredRestaurants()[i].lat;
-		var long = self.filteredRestaurants()[i].long;
+		lat = self.filteredRestaurants()[i].lat;
+		long = self.filteredRestaurants()[i].long;
+		name = self.filteredRestaurants()[i].name;
+		// console.log(name);
+
+		contentString = name;
 
 		// start Google Map marker
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(lat, long),
-			title: self.filteredRestaurants()[i].name,
 			map: map,
-			animation: google.maps.Animation.DROP
+			animation: google.maps.Animation.DROP,
+			clickable: true,
+			textWindow: contentString
 		});
 
-		var contentString = '<div id="content">'+
-	      '<div id="siteNotice">'+
-	      '</div>'+
-	      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-	      '<div id="bodyContent">'+
-	      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-	      'sandstone rock formation in the southern part of the '+
-	      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-	      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-	      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-	      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-	      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-	      'Aboriginal people of the area. It has many springs, waterholes, '+
-	      'rock caves and ancient paintings. Uluru is listed as a World '+
-	      'Heritage Site.</p>'+
-	      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-	      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-	      '(last visited June 22, 2009).</p>'+
-	      '</div>'+
-	      '</div>';
+		var infowindow = new google.maps.InfoWindow({});
 
-	  var infowindow = new google.maps.InfoWindow({
-	      content: contentString
-	  });
+		google.maps.event.addListener(marker, 'click', function(){
+			infowindow.setContent(this.textWindow); 
+			infowindow.open(map,this);
+		});
 
-	  google.maps.event.addListener(marker, 'click', function() {
-	    infowindow.open(map,marker);
-	  });
+		marker.setMap(map); 
 	}
 
 };

5858da7a9cbf64f93d7b05e2faadd4386e9da345
swaption2009 <swaption2009@gmail.com>
2015-07-31 00:36:24 -0700

    Add Yelp oAuth and Search API

--- apps.js
+++ apps.js
@@ -12,6 +12,71 @@ var myList = [
 	{"name":"Ramen Yamadaya","address": "1728 Buchanan St.", "lat":37.786067, "long":-122.429653, "yelpID":"ramen-yamadaya-san-francisco"}
 ];
 
+
+
+
+
+            var auth = {
+                //
+                // Update with your auth tokens.
+                //
+                consumerKey : "lZJQmQK1sk6Z0sxlJh0fkQ",
+                consumerSecret : "QN8BWuHm7Ebahb6xuW6MVkn8Wfs",
+                accessToken : "9Geswp7n67r838vtB4OwyY_fEGmUyCgm",
+                // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+                // You wouldn't actually want to expose your access token secret like this in a real application.
+                accessTokenSecret : "1vzrGIs3U5QPuv1Nu3SXSUUHFJk",
+                serviceProvider : {
+                    signatureMethod : "HMAC-SHA1"
+                }
+            };
+
+            var terms = 'food';
+            var near = 'San+Francisco';
+
+            var accessor = {
+                consumerSecret : auth.consumerSecret,
+                tokenSecret : auth.accessTokenSecret
+            };
+            parameters = [];
+            parameters.push(['term', terms]);
+            parameters.push(['location', near]);
+            parameters.push(['callback', 'cb']);
+            parameters.push(['oauth_consumer_key', auth.consumerKey]);
+            parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+            parameters.push(['oauth_token', auth.accessToken]);
+            parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+            var message = {
+                'action' : 'http://api.yelp.com/v2/search',
+                'method' : 'GET',
+                'parameters' : parameters
+            };
+            console.log(message);
+
+            OAuth.setTimestampAndNonce(message);
+            OAuth.SignatureMethod.sign(message, accessor);
+
+            var parameterMap = OAuth.getParameterMap(message.parameters);
+            console.log(parameterMap);
+
+            $.ajax({
+                'url' : message.action,
+                'data' : parameterMap,
+                'dataType' : 'jsonp',
+                'jsonpCallback' : 'cb',
+                'cache': true,
+                'success' : function(data, textStats, XMLHttpRequest) {
+                    console.log(data);
+                    //$("body").append(output);
+                }
+            });
+
+
+
+
+
+
 // Knockout ViewModel
 var ViewModel = function() {
 	var self = this;
